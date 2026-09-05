a0ed910225e8696d44a896de5a856b16c59b477e
FelixDNguyenn <104798436+FelixOfficials@users.noreply.github.com>
2026-09-05 05:21:28 -0400

    Completed CRUD for Exercise

--- backend/src/controllers/exerciseController.ts
+++ backend/src/controllers/exerciseController.ts
@@ -17,3 +17,26 @@ export const getExercises = async (req: Request, res: Response) => {
     const exercises = await db.orm.public.Exercise.all();
     res.json(exercises);
 }
+
+export const getExercisesById = async (req: Request, res: Response) => {
+    const wantedId = Number(req.params.id);
+    const exercise = await db.orm.public.Exercise.where({id: wantedId}).first();
+    res.json(exercise);
+}
+
+export const updateExercise = async (req: Request, res: Response) => {
+    const wantedId = Number(req.params.id);
+    const updated = await db.orm.public.Exercise.where({id: wantedId}).update( {
+        ...(req.body.name !== undefined && { name: req.body.name }),
+        ...(req.body.muscleGroup !== undefined && { muscleGroup: req.body.muscleGroup }),
+        ...(req.body.equipment !== undefined && { equipment: req.body.equipment })
+    } );
+
+    res.json(updated);
+}
+
+export const deleteExercise = async (req: Request, res: Response) => {
+    const wantedId = Number(req.params.id);
+    await db.orm.public.Exercise.where({id: wantedId}).delete();
+    res.json({ message: "Exercise Deleted"});
+};
--- backend/src/routes/exerciseRoute.ts
+++ backend/src/routes/exerciseRoute.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createExercise, getExercises } from "../controllers/exerciseController";
+import { createExercise, getExercises, getExercisesById, updateExercise, deleteExercise } from "../controllers/exerciseController";
 
 const router = Router();
 
 router.post("/exercises", createExercise);
 router.get("/exercises", getExercises);
+router.get("/exercises/:id", getExercisesById);
+router.put("/exercises/:id", updateExercise);
+router.delete("/exercises/:id", deleteExercise);
 
 export default router;
