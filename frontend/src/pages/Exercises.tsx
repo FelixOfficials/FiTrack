9ee6f2d6ec06ada1a320401426887b842abbac38
FelixDNguyenn <104798436+FelixOfficials@users.noreply.github.com>
2026-09-06 07:16:37 -0400

    Requests and Displays Data on Exercise Page

--- frontend/src/pages/Exercises.tsx
+++ frontend/src/pages/Exercises.tsx
@@ -1,3 +1,30 @@
+import { useState, useEffect } from "react";
+
+type Exercise = {
+    id: number;
+    name: string;
+    muscleGroup: string | null;
+    equipment: string | null;
+}
+
 export default function Exercises() {
-    return <h1>Exercises</h1>
+    const [exercises, setExercises] = useState<Exercise []>([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/exercises").then((res) => res.json()).then((data) => setExercises(data));
+    }, []);
+
+    return (
+        <div>
+            <h1>Exercises</h1>
+
+            <ul>
+                {exercises.map((exercise) => (
+                    <li key={exercise.id}>
+                        {exercise.name} : {exercise.muscleGroup} : {exercise.equipment}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
